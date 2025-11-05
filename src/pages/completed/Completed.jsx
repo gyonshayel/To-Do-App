fc2db26301ada027dd596c884ca692809d9ce17e
Gyon Shayel Joshua <gyon.perera2002@gmail.com>
2025-11-06 00:02:45 +0400

    Implement Completed and Important sections

--- src/pages/completed/Completed.jsx
+++ src/pages/completed/Completed.jsx
@@ -1,10 +1,18 @@
-import { NewTask } from "../../components/NewTask";
-import { TasksList } from "../../components/TasksList";
+import { useTasks } from "../../context/TaskContext";
+import { Task } from "../../components/Task";
 
 export function Completed() {
+  const { tasksArray } = useTasks();
+
+  const filtered = tasksArray.filter((task) => task.status === "complete");
+
   return (
     <>
-      <TasksList list="Completed" />
+      <div className="my-4 space-y-2">
+        {filtered.map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+      </div>
     </>
   );
 }
--- src/pages/important/Important.jsx
+++ src/pages/important/Important.jsx
@@ -1,11 +1,20 @@
+import { useTasks } from "../../context/TaskContext";
+import { Task } from "../../components/Task";
 import { NewTask } from "../../components/NewTask";
-import { TasksList } from "../../components/TasksList";
 
 export function Important() {
+  const { tasksArray } = useTasks();
+
+  const filtered = tasksArray.filter((task) => task.important === true);
+
   return (
     <>
-      <NewTask list="Important" />
-      <TasksList list="Important" />
+      <NewTask />
+      <div className="my-4 space-y-2">
+        {filtered.map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+      </div>
     </>
   );
 }
