13f21b4cccbc86f6f29708a24f34cdcb20b261d3
Gyon Shayel Joshua <gyon.perera2002@gmail.com>
2025-11-06 14:30:28 +0400

    Sort tasks in Scheduled based on the due date

--- src/pages/scheduled/Scheduled.jsx
+++ src/pages/scheduled/Scheduled.jsx
@@ -6,11 +6,18 @@ export function Scheduled() {
   const { tasksArray } = useTasks();
   const today = new Date();
 
-  const filtered = tasksArray.filter(
-    (task) =>
-      new Date(task.date).toISOString().split("T")[0] !==
-        today.toISOString().split("T")[0] || task.list === "Scheduled"
-  );
+  const filtered = tasksArray
+    .filter(
+      (task) =>
+        new Date(task.date).toDateString() !== today.toDateString() ||
+        task.list === "Scheduled"
+    )
+    .sort((a, b) => {
+      // Sort based on the due date
+      if (new Date(a.date).toDateString() === new Date(b.date).toDateString())
+        return 0;
+      return new Date(a.date) < new Date(b.date) ? -1 : 1;
+    });
 
   return (
     <>
