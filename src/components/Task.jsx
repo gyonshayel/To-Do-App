9c2b39f591fdbe2298a97ad0cfbf4e74fbaaf77d
Gyon Shayel Joshua <gyon.perera2002@gmail.com>
2025-11-08 14:24:45 +0400

    Adjust styling in Task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -202,29 +202,10 @@ export function Task({ task }) {
       </div>
 
       {/* Date and Note */}
-      <div className="flex items-center gap-1.5 text-muted-foreground">
-        <div className="pl-2 text-xs sm:text-sm flex gap-1">
-          {task.list === "Scheduled" &&
-            taskDate?.toDateString() === today.toDateString() && (
-              <>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  fill="currentColor"
-                  className="size-4 my-auto"
-                >
-                  <path d="M5.75 7.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM5 10.25a.75.75 0 1 1 1.5 0 .75.75 0 0 1-1.5 0ZM10.25 7.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM7.25 8.25a.75.75 0 1 1 1.5 0 .75.75 0 0 1-1.5 0ZM8 9.5A.75.75 0 1 0 8 11a.75.75 0 0 0 0-1.5Z" />
-                  <path
-                    fillRule="evenodd"
-                    d="M4.75 1a.75.75 0 0 0-.75.75V3a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2V1.75a.75.75 0 0 0-1.5 0V3h-5V1.75A.75.75 0 0 0 4.75 1ZM3.5 7a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v4.5a1 1 0 0 1-1 1h-7a1 1 0 0 1-1-1V7Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <p className="align-baseline">Due Today</p>
-              </>
-            )}
-          {today < taskDate && (
-            <>
+      <div className="flex items-center gap-2 pl-2 text-muted-foreground">
+        {task.list === "Scheduled" &&
+          taskDate?.toDateString() === today.toDateString() && (
+            <div className="text-xs sm:text-sm flex gap-1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 16 16"
@@ -238,13 +219,29 @@ export function Task({ task }) {
                   clipRule="evenodd"
                 />
               </svg>
-              <p className="align-bottom">
-                Due {taskDate.toLocaleDateString("en-US", options)}
-              </p>
-            </>
+              <p className="align-baseline">Due Today</p>
+            </div>
           )}
-        </div>
-        {task.note !== "" && <span>•</span>}
+        {today < taskDate && (
+          <div className="text-xs sm:text-sm flex gap-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="size-4 my-auto"
+            >
+              <path d="M5.75 7.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM5 10.25a.75.75 0 1 1 1.5 0 .75.75 0 0 1-1.5 0ZM10.25 7.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM7.25 8.25a.75.75 0 1 1 1.5 0 .75.75 0 0 1-1.5 0ZM8 9.5A.75.75 0 1 0 8 11a.75.75 0 0 0 0-1.5Z" />
+              <path
+                fillRule="evenodd"
+                d="M4.75 1a.75.75 0 0 0-.75.75V3a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2V1.75a.75.75 0 0 0-1.5 0V3h-5V1.75A.75.75 0 0 0 4.75 1ZM3.5 7a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v4.5a1 1 0 0 1-1 1h-7a1 1 0 0 1-1-1V7Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <p className="align-bottom">
+              Due {taskDate.toLocaleDateString("en-US", options)}
+            </p>
+          </div>
+        )}
 
         {task.note !== "" && (
           <>
